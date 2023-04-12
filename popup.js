a8daaf829a48257747450d90b2cda709d17f764b
Quriyama <57135124+Quriyama@users.noreply.github.com>
2023-04-12 19:53:50 +0900

    Replace 'code' with 'function'

--- popup.js
+++ popup.js
@@ -4,21 +4,19 @@ document.getElementById('downloadPDF').addEventListener('click', async () => {
   await chrome.scripting.executeScript({ target: { tabId: tab.id }, files: ['html2pdf.bundle.min.js'] });
   await chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    code: `
-      (() => {
-        const pdfOptions = {
-          margin: 0,
-          filename: document.title + '.pdf',
-          image: { type: 'jpeg', quality: 0.98 },
-          html2canvas: { scale: 2, logging: false },
-          jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait', compress: true }
-        };
+    function: function() { // Replace 'code' with 'function'
+      const pdfOptions = {
+        margin: 0,
+        filename: document.title + '.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2, logging: false },
+        jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait', compress: true }
+      };
 
-        html2pdf().set(pdfOptions).from(document.body).save().then(() => {
-          window.close();
-        });
-      })();
-    `,
+      html2pdf().set(pdfOptions).from(document.body).save().then(() => {
+        window.close();
+      });
+    }
   });
 
   window.close();
