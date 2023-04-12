3089dd66b6c077c95dfe7e4ee0139e773b52b09a
Quriyama <57135124+Quriyama@users.noreply.github.com>
2023-04-12 18:54:18 +0900

    change logic chrome.scripting.executeScript

--- popup.js
+++ popup.js
@@ -1,34 +1,26 @@
-document.getElementById('downloadPDF').addEventListener('click', () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const tab = tabs[0];
+document.getElementById('downloadPDF').addEventListener('click', async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-    chrome.tabs.executeScript(tab.id, { file: 'jspdf.min.js' }, () => {
-      chrome.tabs.executeScript(tab.id, { file: 'html2pdf.js' }, () => {
-        chrome.tabs.executeScript(
-          tab.id,
-          {
-            code: `
-              (() => {
-                const pdfOptions = {
-                  margin: 0,
-                  filename: document.title + '.pdf',
-                  image: { type: 'jpeg', quality: 0.98 },
-                  html2canvas: { scale: 2, logging: false },
-                  jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait', compress: true }
-                };
+  await chrome.scripting.executeScript({ target: { tabId: tab.id }, files: ['jspdf.min.js'] });
+  await chrome.scripting.executeScript({ target: { tabId: tab.id }, files: ['html2pdf.js'] });
+  await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    code: `
+      (() => {
+        const pdfOptions = {
+          margin: 0,
+          filename: document.title + '.pdf',
+          image: { type: 'jpeg', quality: 0.98 },
+          html2canvas: { scale: 2, logging: false },
+          jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait', compress: true }
+        };
 
-                html2pdf().
-                html2pdf().set(pdfOptions).from(document.body).save().then(() => {
-                  window.close();
-                });
-              })();
-            `,
-          },
-          () => {
-            window.close();
-          }
-        );
-      });
-    });
+        html2pdf().set(pdfOptions).from(document.body).save().then(() => {
+          window.close();
+        });
+      })();
+    `,
   });
+
+  window.close();
 });
